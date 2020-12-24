b27b7c68bc5577074f980f199aeb935924875460
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2020-12-24 18:14:15 -0500

    Add files via upload

---
+++ py_webdev_week1_prog1.py
@@ -0,0 +1,29 @@
+import re
+
+#sample_text_filename = "sample_text.txt"
+actual_text_filename = "actual_text.txt"
+
+#sample_file = open(sample_text_filename, 'r') 
+actual_file = open(actual_text_filename, 'r') 
+#sample_lines = sample_file.readlines() 
+actual_lines = actual_file.readlines()
+
+total_sum = 0
+
+#for line in sample_lines:
+    #nums = re.findall('[0-9]+', line)
+    #for i in range(len(nums)):
+        #num = int(nums[i])
+        #total_sum += num
+    
+#print(sample_text_filename, "value is: ", total_sum)
+
+total_sum = 0
+
+for line in actual_lines:
+    nums = re.findall('[0-9]+', line)
+    for i in range(len(nums)):
+        num = int(nums[i])
+        total_sum += num
+    
+print(actual_text_filename, "value is: ", total_sum)
